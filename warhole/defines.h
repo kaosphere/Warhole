c05b0f7ce18c4f001f43f06350299e0b5f1c6365
phoft <perrine.fauconnier@gmail.com>
2014-01-19 16:41:47 -0500

    add global variables for model types in defines.h

--- warhole/defines.h
+++ warhole/defines.h
@@ -5,4 +5,12 @@
 
 const QString MODEL_PATH("./models");
 
+const QString CAVALERY_STRING("Cavalerie");
+const QString CHARACTER_STRING("Personnage");
+const QString CHARRIOT_STRING("Char");
+const QString INFANTERY_STRING("Infanterie");
+const QString MONSTER_STRING("Monstre");
+const QString WARMACHINE_STRING("Machine de Guerre");
+
+
 #endif // DEFINES_H
