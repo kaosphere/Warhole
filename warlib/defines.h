3fbe92490eb7555eeadc96946aa198bc1d76479a
Alexandre Jumeline <alex.jumeline@gmail.com>
2014-01-21 20:07:33 -0500

    add defines in warlib

---
+++ warlib/defines.h
@@ -0,0 +1,16 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#include <QString>
+
+const QString MODEL_PATH("./models");
+
+const QString CAVALERY_STRING("Cavalerie");
+const QString CHARACTER_STRING("Personnage");
+const QString CHARRIOT_STRING("Char");
+const QString INFANTERY_STRING("Infanterie");
+const QString MONSTER_STRING("Monstre");
+const QString WARMACHINE_STRING("Machine de Guerre");
+
+
+#endif // DEFINES_H
