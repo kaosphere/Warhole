f8d95fc6d3f94a19f083b3a9fbb8570ea9b279f0
Kaosphere <alex.jumeline@gmail.com>
2014-04-03 15:18:27 -0400

    Add documentation for ModelAbstract and StatsModel

--- warlib/model/modelabstract.h
+++ warlib/model/modelabstract.h
@@ -1,3 +1,11 @@
+/*********************************************************************//**
+* modelabstract.h
+* ModelAbstract class
+*
+* AUTEUR: Alexandre Jumeline
+* DATE: 03-04-2014
+*************************************************************************/
+
 #ifndef MODELABSTRACT_H
 #define MODELABSTRACT_H
 
@@ -12,35 +20,130 @@
 #include "paramsfromUI/paramsfromuimodel.h"
 #include "defines.h"
 
-
+//! ModelAbstract class
+/*!
+* Main data structure storing model characteristics.
+* This is the base class for all types of models.
+*/
 class ModelAbstract
 {
 public:
+	//! Constructor.
+	/*!
+	Base constructor of ModelAbstract class
+	*/
     explicit ModelAbstract();
-
+	
+	//! Constructor.
+	/*!
+	* Constructor of ModelAbstract class
+	* \param stat A StatsModel object.
+	* \param widthBase width of the model's base.
+	* \param lengthBase length of the model's base.
+	* \param unitP Unit power of the model
+	* \param url Path of the model image
+	* \param figSup DEPRECATED. TODO: Remove
+	*/
     ModelAbstract(const StatsModel& stat, const int &widthBase,
                   const int &lengthBase, const int &unitP, const QString& url, bool figSup);
-
+	
+	//! A constructor.
+	/*!
+	* Constructor of ModelAbstract class
+	* \param stat A StatsModel object.
+	* \param widthBase width of the model's base.
+	* \param lengthBase length of the model's base.
+	* \param unitP Unit power of the model
+	* \param url Path of the model image
+	* \param figSup DEPRECATED. TODO: Remove
+	* \param n Name of the model
+	* \param move Movement speed
+	* \param weaponS Weapon skills
+	* \param balisticS Balistic skills
+	* \param streangth Strength of the model
+	* \param toughness Toughness of the model
+	* \param wounds Wounds of the model
+	* \param init Initiative of the model
+	* \param attacks Number of attacks of the model
+	* \param leadership Leadership value of the model
+	* \param save Armor save of the model
+	* \param invSave Invulnerable armor save of the model
+	* \param p Points of the model
+	*/
     ModelAbstract(const QString &n, const QString &move, const QString &weaponS,
                   const QString &balisticS, const QString &strength, const QString &toughness,
                   const QString &wounds, const QString &init, const QString &attacks,
                   const QString &leadership, const QString &save, const QString &invSave, const int points, const int &widthBase,
                   const int &lengthBase, const int &unitP, const QString& url, bool figSup);
-
+	
+	//! Copy Constructor.
+	/*!
+	* Copy constructor of ModelAbstract class
+	*/
     ModelAbstract(const ModelAbstract &Copy);
-
+	
+	//! Destructor.
+	/*!
+	* Base destructor of ModelAbstract class
+	*/
     virtual ~ModelAbstract();
-
+	
+	//! Save
+	/*!
+	* VIRTUAL : Method that initiate the saving of the model object in a file.
+	* \param path File path of the file to be saved.
+	*/
     virtual void save(const QString path);
-
+	
+	//! setFromFile
+	/*!
+	* VIRTUAL : Method that loads a model from a previously saved file.
+	* \param path File path of the file to be loaded.
+	*/
     virtual ModelAbstract* setFromFile(const QString path);
+    
+    //! setFromUI
+	/*!
+	* VIRTUAL : Method that loads a model from a form in the UI.
+	* \param params parameter object containing every model possible parameters.
+	*/
     virtual ModelAbstract* setFromUI(const ParamsfromUImodel* params);
+    
+    //! Load
+	/*!
+	* VIRTUAL : Method that initiate the loading of the model object from a file.
+	* \param path File path of the file to be loaded.
+	*/
     virtual void load(const QString path);
     
+    //! displayStringInfo
+	/*!
+	* VIRTUAL Method that returns a string summerizing the values in the object.
+	* \return String containing information.
+	*/
     virtual QString displayStringInfo();
+    
+    //! displayBaseInfo
+	/*!
+	* Method that returns a string summerizing the values in the base class object.
+	* \return String containing information.
+	*/
     QString displayBaseInfo();
     
+    //! getHtml
+	/*!
+	* Virtual Method that returns a string summerizing the values in the object
+	* using HTML format. Used principally to export data as pdf files.
+	* \return String containing information using HTML.
+	*/
     virtual QString getHtml();
+    
+    //! getBaseHtml
+	/*!
+	* Method that returns a string summerizing the values in the base class object
+	* using HTML format. Used principally to export data as pdf files.
+	* \return String containing information using HTML.
+	*/
     QString getBaseHtml();
 
     StatsModel getStats() const;
@@ -63,38 +166,75 @@ public:
 
     QList<OptionModel> getOptions() const;
     void setOptions(const QList<OptionModel> &value);
+    
+    //! addOption
+	/*!
+	* Method that adds an option to the list of options
+	* \param opt option to be added
+	*/
     void addOption(const OptionModel& opt);
+    
+    //! removeOption
+	/*!
+	* Method that removes an option to the list of options
+	* \param opt option to be removed
+	*/
     void removeOption(const OptionModel &opt);
+    
+    //! clearOptions
+	/*!
+	* Method that clears all options of the list of options
+	*/
     void clearOptions();
 
     QString getUrlImage() const;
     void setUrlImage(const QString &value);
 
+	//! clearOptions
+	/*!
+	* Virtual method that computes the global number of points of a model
+	* included chosen options.
+	*/
     virtual int computePoints();
 
 protected:
+
+	//! stats
+	/*!
+	* StatsModel object of the model
+	*/
     StatsModel stats;
     //QList<ModelAbstract *> champion; //list of possible champion
 
-    int squareBaseW;    //width of the square base
-    int squareBaseL;    //length of the square base
+    int squareBaseW;    /// Width of the square base
+    int squareBaseL;    /// Length of the square base
 
-    int unitPower;      //unit power of the model
+    int unitPower;      /// Unit power of the model
 
-    bool figSupInd;     //independant models (for charriots, war machines and monsters)
+    bool figSupInd;     /// Independant models (for charriots, war machines and monsters)
 
-    QPixmap *image;     //sprite of the model to be used in the graphics
-    QString urlImage;
+    QPixmap *image;     /// Image of the model to be used in the graphics
+    QString urlImage;	/// Path to the image
 
-    QList<OptionModel> options;   //list of options possible for model
-
+    QList<OptionModel> options;   /// List of options possible for model
+	
+	//! QDataStream & operator <<
+	/*!
+	* Friend QDataStream operator << used to serialize data and save files.
+	* \param QDataStream& stream where the data will be inputed.
+	* \param ModelAbstract& object to be serialized.
+	* \return The stream containing the data.
+	*/
     friend QDataStream & operator << (QDataStream &, const ModelAbstract &);
-    friend QDataStream & operator >> (QDataStream &, ModelAbstract &);
     
-public slots:
-
-signals:
-    
+    //! QDataStream & operator >>
+	/*!
+	* Friend QDataStream operator >> used to serialize data and load files.
+	* \param QDataStream& stream from where the data will be outputed.
+	* \param ModelAbstract& object to be serialized.
+	* \return The stream containing the data.
+	*/
+    friend QDataStream & operator >> (QDataStream &, ModelAbstract &);   
 };
 
 #endif // MODELABSTRACT_H
--- warlib/stats/statsmodel.h
+++ warlib/stats/statsmodel.h
@@ -1,3 +1,11 @@
+/*********************************************************************//**
+* modelabstract.h
+* StatsModel class
+*
+* AUTEUR: Alexandre Jumeline
+* DATE: 03-04-2014
+*************************************************************************/
+
 #ifndef STATSMODEL_H
 #define STATSMODEL_H
 
@@ -7,82 +15,220 @@
 #include <QDataStream>
 #include <QTextStream>
 
+
+//! StatsModel class
+/*!
+* This class holds all the common attributes of a model.
+*/
 class StatsModel
 {
 public:
+	//! Constructor.
+	/*!
+	* Base constructor of StatsModel class
+	*/
     StatsModel();
+    
+    //! Copy Constructor.
+	/*!
+	* Copy constructor of StatsModel class
+	*/
     StatsModel(const StatsModel&);
+    
+    //! Constructor.
+	/*!
+	* Constructor of StatsModel class
+	* \param n Name of the model
+	* \param move Movement speed
+	* \param weaponS Weapon skills
+	* \param balisticS Balistic skills
+	* \param streangth Strength of the model
+	* \param toughness Toughness of the model
+	* \param wounds Wounds of the model
+	* \param init Initiative of the model
+	* \param attacks Number of attacks of the model
+	* \param leadership Leadership value of the model
+	* \param save Armor save of the model
+	* \param invSave Invulnerable armor save of the model
+	* \param p Points of the model
+	*/
     StatsModel(const QString &n, const QString &move, const QString &weaponS, const QString &balisticS,
                const QString &strength, const QString &toughness, const QString &wounds,
                const QString &init, const QString &attacks, const QString &leadership,
                const QString &save, const QString &invSave, int p);
-
+	
+	//! Destructor.
+	/*!
+	* Base destructor of StatsModel class
+	*/
     ~StatsModel();
-
+	
+	//! operator=
+	/*!
+	* Overload of operator= for StatsModel class
+	* \param s StatsModel whose values will be assigned from.
+	*/
     StatsModel& operator=(const StatsModel&);
-
+	
+	//! operator==
+	/*!
+	* Overload of operator== for StatsModel class
+	* \param s StatsModel whose values will be compared to.
+	*/
     bool operator==(const StatsModel&);
-
+	
+	//! displayString
+	/*!
+	* Method that returns a string summerizing the values in the object.
+	* \return String containing information.
+	*/
     QString displayString() const;
     
+    //! getHtml
+	/*!
+	* Virtual Method that returns a string summerizing the values in the object
+	* using HTML format. Used principally to export data as pdf files.
+	* \return String containing information using HTML.
+	*/
     QString getHtml() const;
-
+	
+	//! getName
+	/*!
+	* get accessor for name value.
+	* \return name Returns the name of the model.
+	*/
     QString getName() const;
     void setName(const QString &value);
 
+	//! getM
+	/*!
+	* get accessor for m value.
+	* \return name Returns the movement speed of the model.
+	*/
     QString getM() const;
     void setM(const QString &value);
-
+	
+	//! getWs
+	/*!
+	* get accessor for ws value.
+	* \return name Returns the movement weapon skills of the model.
+	*/
     QString getWs() const;
     void setWs(const QString &value);
 
+	//! getBs
+	/*!
+	* get accessor for bs value.
+	* \return name Returns the movement balistic skills of the model.
+	*/
     QString getBs() const;
     void setBs(const QString &value);
 
+	//! getS
+	/*!
+	* get accessor for ws value.
+	* \return name Returns the movement strength of the model.
+	*/
     QString getS() const;
     void setS(const QString &value);
-
+	
+	//! getT
+	/*!
+	* get accessor for ws value.
+	* \return name Returns the toughness of the model.
+	*/
     QString getT() const;
     void setT(const QString &value);
-
+	
+	//! getW
+	/*!
+	* get accessor for ws value.
+	* \return name Returns the wounds of the model.
+	*/
     QString getW() const;
     void setW(const QString &value);
-
+	
+	//! getI
+	/*!
+	* get accessor for ws value.
+	* \return name Returns the initiative of the model.
+	*/
     QString getI() const;
     void setI(const QString &value);
-
+	
+	//! getA
+	/*!
+	* get accessor for ws value.
+	* \return name Returns the number of attacks of the model.
+	*/
     QString getA() const;
     void setA(const QString &value);
 
+	//! getLd
+	/*!
+	* get accessor for ws value.
+	* \return name Returns the leadership of the model.
+	*/
     QString getLd() const;
     void setLd(const QString &value);
-
+	
+	//! getSvg
+	/*!
+	* get accessor for ws value.
+	* \return name Returns the armor save of the model.
+	*/
     QString getSvg() const;
     void setSvg(const QString &value);
-
+	
+	//! getSvgInv
+	/*!
+	* get accessor for ws value.
+	* \return name Returns the invulnerable armor save of the model.
+	*/
     QString getSvgInv() const;
     void setSvgInv(const QString &value);
-
+	
+	//! getPoints
+	/*!
+	* get accessor for ws value.
+	* \return name Returns the points of the model.
+	*/
     int getPoints() const;
     void setPoints(int value);
 
 protected:
-    QString name;
-    QString m;      //movement
-    QString ws;     //weapon skill
-    QString bs;     //balistic skill
-    QString s;      //strength
-    QString t;      //toughtness
-    QString w;      //wounds
-    QString i;      //initiative
-    QString a;      //attacks
-    QString ld;     //leadership
-    QString svg;    //armor save
-    QString svgInv; //invulnerable armor save
 
-    int points;     //points of the model
+    QString name;	/// Name of the model
+    QString m;      /// Movement of the model
+    QString ws;     /// Weapon skill of the model
+    QString bs;     /// Balistic skill of the model
+    QString s;      /// Strength of the model
+    QString t;      /// Toughtness of the model
+    QString w;      /// Wounds of the model
+    QString i;      /// Initiative of the model
+    QString a;      /// Attacks of the model
+    QString ld;     /// Leadership of the model
+    QString svg;    /// Armor save of the model
+    QString svgInv; /// Invulnerable armor save of the model
 
+    int points;     /// Points of the model
+
+	//! QDataStream & operator <<
+	/*!
+	* Friend QDataStream operator << used to serialize data and save files.
+	* \param QDataStream& stream where the data will be inputed.
+	* \param StatsModel& object to be serialized.
+	* \return The stream containing the data.
+	*/
     friend QDataStream & operator << (QDataStream &, const StatsModel &);
+    
+    //! QDataStream & operator >>
+	/*!
+	* Friend QDataStream operator >> used to serialize data and load files.
+	* \param QDataStream& stream from where the data will be outputed.
+	* \param StatsModel& object to be serialized.
+	* \return The stream containing the data.
+	*/
     friend QDataStream & operator >> (QDataStream &, StatsModel &);
 
 };
