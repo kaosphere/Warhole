a981bc27a78b13a1e86a3cc60f635758be839c05
Alexandre Jumeline <alex.jumeline@gmail.com>
2014-05-03 20:58:57 -0400

    Fixes in factory

--- warlib/model/modelfactory.cpp
+++ warlib/model/modelfactory.cpp
@@ -33,11 +33,11 @@ ModelAbstract *ModelFactory::createEmptyModel(const QString &key)
     QMap<QString, ModelAbstract*>::const_iterator it = factory_model_map.find(key);
     if(it != factory_model_map.end()) // key was found in map
     {
-        QLog_Info(LOG_ID_INFO, "createEmptyModel() : Key found in map : " + s);
+        QLog_Info(LOG_ID_INFO, "createEmptyModel() : Key found in map : " + key);
         tmp = (*it)->clone();
     }
     else
-        QLog_Error(LOG_ID_ERR, "createEmptyModel() : Can't find key in path : " + path);
+        QLog_Error(LOG_ID_ERR, "createEmptyModel() : Can't find key : " + key);
     return tmp;
 }
 
@@ -70,10 +70,10 @@ ModelAbstract* ModelFactory::createFromUI(const QString &key, const ParamsfromUI
     {
         tmp = (*it)->clone();
         tmp = tmp->setFromUI(params);
-        QLog_Info(LOG_ID_INFO, "createFromUI() : Key found in map : " + s);
+        QLog_Info(LOG_ID_INFO, "createFromUI() : Key found in map : " + key);
     }
     else
-        QLog_Error(LOG_ID_ERR, "createFromFile() : Can't find key in path : " + path);
+        QLog_Error(LOG_ID_ERR, "createFromFile() : Can't find key : " + key);
 
     return tmp;
 }
