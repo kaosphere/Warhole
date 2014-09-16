17263c2b199984ed3e5e751f1b72ef41b28a5911
Kaosphere <alex.jumeline@gmail.com>
2014-09-16 16:52:52 -0400

    enhancedgraphicsView documentation comments

--- warhole/graphics/enhancedgraphicsview.h
+++ warhole/graphics/enhancedgraphicsview.h
@@ -1,3 +1,11 @@
+/*! ********************************************************************
+* EnhancedGraphicsView.h
+* EnhancedGraphicsView class
+*
+* AUTEUR: Alexandre Jumeline
+* DATE: 16-09-2014
+*************************************************************************/
+
 #ifndef ENHANCEDGRAPHICSVIEW_H
 #define ENHANCEDGRAPHICSVIEW_H
 
@@ -8,25 +16,71 @@
 #include <QDebug>
 #include "Utilities/QLogger/QLogger.h"
 
+//! EnhancedGraphicsView class
+/*!
+* This class inheritates QGraphicsView and enhance it with
+* a zoom using the mouse wheel.
+*/
 class EnhancedGraphicsView : public QGraphicsView
 {
     Q_OBJECT
 public:
+    //! Constructor.
+    /*!
+    * Base constructor of EnhancedGraphicsView class
+    */
     explicit EnhancedGraphicsView(QWidget *parent=0);
+
+    //! Destructor.
+    /*!
+    * Destructor of EnhancedGraphicsView class
+    */
     virtual ~EnhancedGraphicsView();
 
 public slots:
+    //! zoomIn.
+    /*!
+    * Slot that zooms in the view.
+    */
     void zoomIn();
+
+    //! zoomOut.
+    /*!
+    * Slot that zooms out from the view.
+    */
     void zoomOut();
 protected:
-    void wheelEvent(QWheelEvent *event);
+    //! wheelEvent.
+    /*!
+    * Reimplementation of the mouse wheel event from QGraphicsView
+    * to perform a zoom instead of scrolling the view.
+    */
+    virtual void wheelEvent(QWheelEvent *event);
 
 private:
+    //! scaleBy.
+    /*!
+    * Function that performs the scaling
+    * @param factor Factor of the scaling
+    */
     void scaleBy(double factor);
 
+    //! scaleFactor.
+    /*!
+    * Factor of scaling applied currently to the view.
+    */
     double scaleFactor;
 
+    //! MAX_SCALE_FACTOR.
+    /*!
+    * Maximum factor of scaling that can be applied to the view.
+    */
     static const double MAX_SCALE_FACTOR;
+
+    //! MIN_SCALE_FACTOR.
+    /*!
+    * Minimum factor of scaling that can be applied to the view.
+    */
     static const double MIN_SCALE_FACTOR;
 
     static const QString LOG_ID_TRACE;
