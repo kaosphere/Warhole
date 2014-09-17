3c5d993db2a61133631f9514736b0e148b0c07fb
Kaosphere <alex.jumeline@gmail.com>
2014-09-17 17:04:44 -0400

    GraphicRegiment progress

--- warhole/graphics/regimentgraphics.cpp
+++ warhole/graphics/regimentgraphics.cpp
@@ -1,5 +1,72 @@
 #include "regimentgraphics.h"
 
+using namespace QLogger;
+
+const QString RegimentGraphics::LOG_ID_INFO = "RegimentGraphics_info";
+const QString RegimentGraphics::LOG_ID_TRACE = "RegimentGraphics_trace";
+const QString RegimentGraphics::LOG_ID_WARN = "RegimentGraphics_warm";
+const QString RegimentGraphics::LOG_ID_ERR = "RegimentGraphics_err";
+
 RegimentGraphics::RegimentGraphics()
 {
+    initRegimentGraphics();
 }
+
+RegimentGraphics::RegimentGraphics(const RegimentAbstract &r)
+{
+    initRegimentGraphics();
+
+    regiment = r;
+}
+
+void RegimentGraphics::initRegimentGraphics()
+{
+    QLoggerManager *manager = QLoggerManager::getInstance();
+    manager->addDestination("./logs/lastrun.log", QStringList(LOG_ID_TRACE), QLogger::TraceLevel);
+    manager->addDestination("./logs/lastrun.log", QStringList(LOG_ID_INFO), QLogger::InfoLevel);
+    manager->addDestination("./logs/lastrun.log", QStringList(LOG_ID_ERR), QLogger::ErrorLevel);
+    manager->addDestination("./logs/lastrun.log", QStringList(LOG_ID_WARN), QLogger::WarnLevel);
+
+    // Redraw regiment if the regimentAbstract object is replaced
+    QObject::connect(this, SIGNAL(regimentUpdated()), this, SLOT(updateRegiment()));
+}
+
+QRectF RegimentGraphics::boundingRect() const
+{
+
+}
+
+void RegimentGraphics::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+
+}
+
+void RegimentGraphics::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
+{
+    QLog_Info(LOG_ID_INFO, "X : " + QString::number(scenePos().x()) +
+                           ", Y : " + QString::number(scenePos().y()));
+
+    // Signal that the object has been moved by the mouse
+    // This permits not to create infinite loop if we cast the signal
+    // when the coordinates are changed
+    emit(SIGNAL(regimentMoved()));
+
+    QGraphicsItem::mouseMoveEvent(event);
+}
+
+void RegimentGraphics::updateRegiment()
+{
+    this->update();
+}
+
+RegimentAbstract RegimentGraphics::getRegiment() const
+{
+    return regiment;
+}
+
+void RegimentGraphics::setRegiment(const RegimentAbstract &value)
+{
+    regiment = value;
+    emit(SIGNAL(regimentChanged()));
+}
+
--- warhole/graphics/regimentgraphics.h
+++ warhole/graphics/regimentgraphics.h
@@ -3,14 +3,64 @@
 
 #include <QGraphicsItem>
 
-class RegimentGraphics : public QGraphicsItem
+#include "game/regiment/regimentabstract.h"
+#include "Utilities/QLogger/QLogger.h"
+
+class RegimentGraphics : public QGraphicsObject
 {
+    Q_OBJECT
 public:
     RegimentGraphics();
+    RegimentGraphics(const RegimentAbstract& r);
     virtual ~RegimentGraphics();
 
     QRectF boundingRect() const;
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+
+    RegimentAbstract getRegiment() const;
+    void setRegiment(const RegimentAbstract &value);
+
+protected:
+    virtual void mouseMoveEvent(QGraphicsSceneMouseEvent* event);
+
+private:
+    void initRegimentGraphics();
+
+    static const QString LOG_ID_TRACE;
+    static const QString LOG_ID_INFO;
+    static const QString LOG_ID_WARN;
+    static const QString LOG_ID_ERR;
+
+    RegimentAbstract regiment;
+
+    QList<QGraphicsItem> models;
+
+
+private slots:
+    void updateRegiment();
+
+signals:
+    //! regimentDataChanged.
+    /*!
+    * This signal is sent when a value has changed in the regiment.
+    */
+    void regimentDataChanged();
+
+    //! regimentMoved.
+    /*!
+    * This signal is sent when the graphic regiment is moved.
+    */
+    void regimentMoved();
+
+    //! regimentUpdated.
+    /*!
+    * This signal is casted when a new regiment is set in place of the previous
+    * one. This signal is different from regimentDataChanged() because we don't
+    * want to repaint the graphical object each time we change a value in the regiment.
+    * But when a new regiment is set, it means that the display has to be updated.
+    */
+    void regimentUpdated();
+
 };
 
 #endif // REGIMENTGRAPHICS_H
