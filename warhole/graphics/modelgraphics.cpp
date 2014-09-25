3e1c9277279b50793b48a5d52f347f9f04559450
Kaosphere <alex.jumeline@gmail.com>
2014-09-25 18:28:49 -0400

    Implement ModelGraphics

--- warhole/graphics/modelgraphics.cpp
+++ warhole/graphics/modelgraphics.cpp
@@ -1,5 +1,28 @@
 #include "modelgraphics.h"
 
-ModelGraphics::ModelGraphics()
+ModelGraphics::ModelGraphics(QGraphicsItem* parent):
+    QGraphicsRectItem(parent)
 {
 }
+
+ModelGraphics::ModelGraphics(int x, int y, int w, int h, QString n,QGraphicsItem* parent):
+    QGraphicsRectItem(x,y,w,h,parent)
+{
+    modelName = n;
+}
+
+ModelGraphics::~ModelGraphics()
+{
+
+}
+
+QString ModelGraphics::getModelName() const
+{
+    return modelName;
+}
+
+void ModelGraphics::setModelName(const QString &value)
+{
+    modelName = value;
+}
+
--- warhole/graphics/modelgraphics.h
+++ warhole/graphics/modelgraphics.h
@@ -3,10 +3,25 @@
 
 #include <QGraphicsRectItem>
 
+
+//! ModelGraphics class
+/*!
+* This class inheritates QGraphicsRectItem and enhance it with
+* the name of the model that it's represented.
+*/
 class ModelGraphics : public QGraphicsRectItem
 {
 public:
-    ModelGraphics();
+    ModelGraphics(QGraphicsItem *parent = 0);
+    ModelGraphics(int x, int y, int w, int h, QString n, QGraphicsItem *parent = 0);
+
+    virtual ~ModelGraphics();
+
+    QString getModelName() const;
+    void setModelName(const QString &value);
+
+private:
+    QString modelName;
 };
 
 #endif // MODELGRAPHICS_H
