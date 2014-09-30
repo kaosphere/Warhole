47a3bc84358a2c6d50fa2f9ec13c0409ad0baf08
Kaosphere <alex.jumeline@gmail.com>
2014-09-30 17:36:33 -0400

    Add millimeter to pixel conversion distance

--- warhole/graphics/distances.h
+++ warhole/graphics/distances.h
@@ -2,6 +2,8 @@
 #define DISTANCES_H
 
 //convert distances into pixels
+#define ONE_INCH_IN_MILLIMETERS 25.4
+#define ONE_MILLIMETER 3
 #define ONE_INCH 75  //base conversion : one inch = 75 pixels
 #define TWELVE_INCHES 12*ONE_INCH
 #define ONE_FOOT TWELVE_INCHES
