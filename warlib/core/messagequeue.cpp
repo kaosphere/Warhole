b8b076757d81c8cafefaeab43917b95880f3a3e4
Alexandre Jumeline <alex.jumeline@gmail.com>
2014-10-06 22:59:22 -0400

    Remove mutex as network won't be in a separate thread anymore

--- warlib/core/messagequeue.cpp
+++ warlib/core/messagequeue.cpp
@@ -7,23 +7,25 @@ MessageQueue::MessageQueue(QObject *parent) :
 
 QList<Message> MessageQueue::getMessageList()
 {
-    QMutexLocker locker(&mutex);
+    //QMutexLocker locker(&mutex);
     return messageList;
 }
 
 void MessageQueue::setMessageList(const QList<Message> &value)
 {
-    QMutexLocker locker(&mutex);
+    //QMutexLocker locker(&mutex);
     messageList = value;
 }
 
 void MessageQueue::addMessage(const Message &m)
 {
-    // Protect the message list
-    QMutexLocker locker(&mutex);
+    {
+        // Protect the message list
+        //QMutexLocker locker(&mutex);
 
-    // Add new message
-    messageList.append(m);
+        // Add new message
+        messageList.append(m);
+    }
 
     // Emit signal so that the new message can be processed
     emit newMessageAvailable();
@@ -31,19 +33,19 @@ void MessageQueue::addMessage(const Message &m)
 
 void MessageQueue::removeMessage(Message &m)
 {
-    QMutexLocker locker(&mutex);
+    //QMutexLocker locker(&mutex);
     messageList.removeOne(m);
 }
 
 void MessageQueue::clearMessages()
 {
-    QMutexLocker locker(&mutex);
+    //QMutexLocker locker(&mutex);
     messageList.clear();
 }
 
 Message MessageQueue::getAndRemoveFirstMessage()
 {
-    QMutexLocker locker(&mutex);
+    //QMutexLocker locker(&mutex);
     Message m = messageList.first();
     messageList.removeOne(m);
     return m;
